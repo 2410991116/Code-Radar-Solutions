de08c6f1a501d6ff99bc31c4e4bbaa86ca99e9c0
2410991116 <jasnoor1116.becse24@chitkara.edu.in>
2025-02-04 17:26:16 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -3,13 +3,13 @@
 int main() {
     int a, b, c;
 
-    printf("Enter the three sides of the triangle: ");
+    
     scanf("%d %d %d", &a, &b, &c);
 
     if (a + b > c && a + c > b && b + c > a) {
-        printf("The sides %d, %d, and %d can form a triangle.\n", a, b, c);
+        printf("Valid");
     } else {
-        printf("The sides %d, %d, and %d cannot form a triangle.\n", a, b, c);
+        printf("Invalid");
     }
 
     return 0;
