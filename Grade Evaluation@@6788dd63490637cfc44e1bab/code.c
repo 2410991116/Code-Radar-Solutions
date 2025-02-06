f0f2897841d6361f44dd97e36da1dc018355c296
2410991116 <jasnoor1116.becse24@chitkara.edu.in>
2025-02-06 23:39:33 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    char a;
+    scanf("%f",&a);
+    if (a==A){
+        printf("Exellent");
+    }else if(a==B){
+        printf("Good");
+    }else if(a==C){
+        printf("Average");
+    }else if(a==D){
+        printf("Below Average");
+    } else if(a==F){
+        printf("Fali");
+    }
+}
