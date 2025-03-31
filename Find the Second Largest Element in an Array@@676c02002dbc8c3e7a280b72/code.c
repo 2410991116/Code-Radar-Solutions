8e6bae32030b0178da65d1ca53505a700e1b41d5
Jasnoor Singh <jasnoortoor132@gmail.com>
2025-04-01 00:44:56 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,52 +1,45 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+int bubble(int arr[],int n){
+    for (int i=0;i<size-1;i++){
+        for (int j=0;j<size-i-1;j++){
+            if (arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
         }
+
     }
 }
 
-int findSecondLargestUsingBubbleSort(int arr[], int size) {
-    if (size < 2) {
-        return -1; // Invalid case: array size is less than 2
+int main(){
+    int size;
+    scanf("%d",&size);
+
+    int arr[size];
+    for (int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
     }
 
-    bubbleSort(arr, size); // Sort the array using bubble sort
+    bubble(arr,size);
 
-    // Find the second largest element
-    int largest = arr[size - 1];
-    for (int i = size - 2; i >= 0; i--) {
-        if (arr[i] < largest) {
-            return arr[i]; // Found the second largest
+    if (size==1){
+        printf("-1");
+    }
+
+    int largest=arr[size-1];
+    int second=-1;
+    for (int i=n-2;i>0;i++){
+        if (arr[i]!=largest){
+            second=arr[i];
+            break;
         }
     }
-    return -1; // No valid second largest found (e.g., all elements are identical)
-}
 
-int main() {
-    int n;
-    
-    scanf("%d", &n);
-
-    int arr[n];
-   
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    if (second==-1){
+        printf("-1");
+    }else{
+        printf("%d",second);
     }
-
-    int result = findSecondLargestUsingBubbleSort(arr, n);
-    if (result == -1) {
-        printf("-1\n");
-    } else {
-        printf("%d\n", result);
-    }
-
-    return 0;
 }
