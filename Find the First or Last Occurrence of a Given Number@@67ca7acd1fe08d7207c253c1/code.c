4996ae746eca162d4bbd5ed1f9560d94b29967f7
Jasnoor Singh <jasnoortoor132@gmail.com>
2025-04-08 11:03:51 +0530

    Solution to Find the First or Last Occurrence of a Given Number

--- Find the First or Last Occurrence of a Given Number@@67ca7acd1fe08d7207c253c1/code.c
+++ Find the First or Last Occurrence of a Given Number@@67ca7acd1fe08d7207c253c1/code.c
@@ -1,44 +1,41 @@
 #include <stdio.h>
 
-int bubbleSort(int arr[],int n ){
-    for (int i=0;i<n-1;i++){
-        for (int j=0;j<n-i-1;j++){
-            if (arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-
-    
 }
 
-int findOccurrence(int arr[], int n, char mode, int t) {   
-    
-    bubbleSort(arr, n);
-    
-    
 
-    if (t<0  || t>=n){
-        return -1;
+int findOccurrence(int arr[], int n, char mode, int t) {    
+    bubbleSort(arr, n); 
+
+    if (t < 0 || t >= n) {
+        return -1; 
     }
-    
+
     if (mode == 'F') {
         for (int i = 0; i < n; i++) {
             if (arr[i] == t) {
-                return i;
-                break; 
+                return i; 
             }
         }
-    }
-    else if (mode == 'L') {
+    } else if (mode == 'L') {
         for (int i = n - 1; i >= 0; i--) {
             if (arr[i] == t) {
                 return i;
-                break;
             }
         }
-    }
+    } else {
+        return -1; 
 
+    return -1; 
 }
+
