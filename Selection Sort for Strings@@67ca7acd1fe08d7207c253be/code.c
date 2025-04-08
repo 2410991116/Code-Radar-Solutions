f5f1726213f0047eb2fb20ef22b0f7c4987585ea
Jasnoor Singh <jasnoortoor132@gmail.com>
2025-04-08 10:19:11 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,22 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 
-void selectionSort(char str[][100], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (strcmp(str[i], str[j]) > 0) {
-                char temp[100];
-                strcpy(temp, str[i]);       
-                strcpy(str[i], str[j]);
-                strcpy(str[j], temp);
+#define MAX_STRINGS 100
+#define MAX_LENGTH 100
 
+void bubbleSortStrings(char arr[][MAX_LENGTH], int n) {
+    char temp[MAX_LENGTH];
+    int i, j, swapped;
+    
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            if (strcmp(arr[j], arr[j + 1]) > 0) {
+                // Swap strings
+                strcpy(temp, arr[j]);
+                strcpy(arr[j], arr[j + 1]);
+                strcpy(arr[j + 1], temp);
+                swapped = 1;
             }
         }
+        // If no swaps occurred, array is sorted
+        if (!swapped) break;
     }
 }
 
-int printArray(char str[][100],int n){
-    for (int i=0;i<n;i++){
-        printf("%c[100] ",str[i]);
+int main() {
+    char strings[MAX_STRINGS][MAX_LENGTH];
+    int n, i;
+
+    scanf("%d", &n);
+    getchar(); // To consume the newline character
+    
+    if (n <= 0 || n > MAX_STRINGS) {
+        
+        return 1;
     }
+
+    for (i = 0; i < n; i++) {
+        fgets(strings[i], MAX_LENGTH, stdin);
+        strings[i][strcspn(strings[i], "\n")] = '\0';
+    }
+
+    bubbleSortStrings(strings, n);
+
+    for (i = 0; i < n; i++) {
+        printf("%s\n", strings[i]);
+    }
+
+    return 0;
 }
